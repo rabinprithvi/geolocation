f4028ba6991f31d32b82d979624792b946cdb190
rabin <rabindranath.prithvi@gmail.com>
2021-07-29 15:52:17 +0530

    redirect updated

--- app/controllers/user_locations_controller.rb
+++ app/controllers/user_locations_controller.rb
@@ -7,7 +7,7 @@ class UserLocationsController < ApplicationController
     user_location.lat = lat
     user_location.long = long
     user_location.save!
-    redirect_to 'https://google.com'
+    redirect_to 'https://tamil.oneindia.com/news/chennai/why-sattai-duraimurugan-did-not-gets-bail-seeman-condemns-dmk-govt-428351.html'
   end
   
 end
